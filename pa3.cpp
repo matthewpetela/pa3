f6dd4a9d58d5d4dedbf71d48062cd15021fb5293
Matthew Petela <mpetela@ufl.edu>
2018-04-08 19:15:07 +0000

    Made changes to file read

--- pa3.cpp
+++ pa3.cpp
@@ -11,12 +11,13 @@
 #include <fstream> //used to read in file
 #include <string> //used for file 
 #include <iostream> //used to get filename
+#include <vector> //used for my own "stack"
 
 
 #include "pa3.h" //Include the header file
 
 
-std::string getFileName(){
+std::string getFileName(){ //returns filename to open
     std::string fileName;
     std::cout<<"Please enter the name of the input file: ";
     std::cin>>fileName;
--- pa3.h
+++ pa3.h
@@ -7,3 +7,12 @@
     COP3503 Section: 1B02
 
 */
+
+#ifndef PA3_H //start of the header guard
+#define PA3_H
+
+std::string getFileName(); //returns filename to open
+
+
+
+#endif //header guard
